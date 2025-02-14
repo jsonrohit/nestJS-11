3f07fd75f43d889c2e2e96043e97dba850d60e39
jsonnrohit <rohit050996@gmail.com>
2025-02-14 19:01:14 +0530

    setup prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the config available globally
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,PrismaService],
+  exports: [PrismaService], // Export PrismaService to use in other modules
 })
 
-export class AppModule {}
+export class AppModule { }
