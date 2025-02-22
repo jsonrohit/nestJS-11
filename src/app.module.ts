f98fc0e7942074603839558b0c0bc979abb3ec4e
Rohit Kumar <rohit.kumar@13sqft.com>
2025-02-22 20:47:03 +0530

    working user api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true, // Makes the config available globally
-    })
+    }),
+    UserModule
   ],
-  controllers: [AppController],
+  controllers: [AppController, UserController],
   providers: [AppService,PrismaService],
   exports: [PrismaService], // Export PrismaService to use in other modules
 })
