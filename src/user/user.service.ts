9e9945195d7a21fe5d207d4c2258907973b2ea81
Rohit Kumar <rohit.kumar@13sqft.com>
2025-02-23 14:32:41 +0530

    add record method implementation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService){
+
+  }
   create(createUserDto: CreateUserDto) {
+    console.log(createUserDto,'createUserDto');
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+      },
+    });
     return 'This action adds a new user';
   }
 
