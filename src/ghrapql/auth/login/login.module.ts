22d808ae69243ea4357875965256a3718b8bfbc1
Rohit Kumar <rohit.kumar@13sqft.com>
2025-03-20 23:43:55 +0530

    working on ghrapql

--- src/ghrapql/auth/login/login.module.ts
+++ src/ghrapql/auth/login/login.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginResolver } from './login.resolver';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
+  imports:[
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+  ],
   providers: [LoginResolver, LoginService],
 })
 export class LoginModule {}
